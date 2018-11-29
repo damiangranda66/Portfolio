f407d7ddba4485aa39bd1ca0ea40b0e5b86fe5f2
aniaska4 <aniaska4@wp.pl>
2018-11-29 21:38:05 +0100

    first repo

---
+++ app.js
@@ -0,0 +1,17 @@
+
+//     let btn = document.querySelector('.toggle')
+//     btn.addEventListener('click',() => {
+//         document.querySelector(".bar1").classList.toggle("change");
+//     })
+
+//    //hamburger
+
+//      const hamburger = document.querySelector('.toggle');
+           
+//     hamburger.addEventListener('click', updateValue);
+
+//     function updateValue() {
+//          document.querySelector('.dropdown').classList.toggle('active');
+//     }
+
+
