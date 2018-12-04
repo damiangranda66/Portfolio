49aee11f1802b95c9b8d124a8067eb3865ed6d68
aniaska4 <aniaska4@wp.pl>
2018-12-04 22:27:56 +0100

    add footer

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
+//projects
+document.addEventListener('DOMContentLoaded', function() {
+    let card = document.querySelectorAll('.projects_pic');
 
-//     let btn = document.querySelector('.toggle')
-//     btn.addEventListener('click',() => {
-//         document.querySelector(".bar1").classList.toggle("change");
-//     })
+    for(let el of card){
+    console.log(card)
+        el.addEventListener('mouseover',  () =>{
+            el.style.boxShadow = "-1px 9px 40px -1px black";
+            el.style.marginTop = "2px";
+            el.style.transition = "margin-top 0.3s ease";
+        })
 
-//    //hamburger
+        el.addEventListener('mouseout',  () =>{
+            el.style.boxShadow = "none";
+            el.style.marginTop = "10px";
+            el.style.transition = "margin-top 0.3s ease";
+        })
+    }
+    
 
-//      const hamburger = document.querySelector('.toggle');
-           
-//     hamburger.addEventListener('click', updateValue);
+});
 
-//     function updateValue() {
-//          document.querySelector('.dropdown').classList.toggle('active');
-//     }
+
+
 
 
